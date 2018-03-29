571d205c3b43c95a512a9a2008b768b9084d68fd
zghiche <zghiche@cern.ch>
2018-03-29 11:48:36 +0200

    Update new.sh

--- new.sh
+++ new.sh
@@ -8,7 +8,8 @@ echo ToRun/$file
     week=`grep "week" ToRun/$file | awk '{print $2}'`
     sqlite1=`grep "run1" ToRun/$file | awk '{print $2}'`
     sqlite2=`grep "run2" ToRun/$file | awk '{print $2}'`
-mv ToRun/$file RunFiles/.
+cp ToRun/$file RunFiles/.
+rm ToRun/$file
 echo "./L1RateValidation.sh $sqlite1 $sqlite2 $week $year"
 ./L1RateValidation.sh $sqlite1 $sqlite2 $week $year 
 git commit -a -m "clean ToRun files"
